596eba0f4e0dd411d26dce8aa86489edaee6338d
jasminericka <jasmin.celebre@gmail.com>
2023-04-21 06:06:15 +0800

    convert key to numbers

--- vigenere_cipher.py
+++ vigenere_cipher.py
@@ -4,7 +4,11 @@
 #ask user for message and key then save it
 message = input("Please type the message(all uppercase letters, no spaces): ").strip()
 key = input("Please type the key (all uppercase letters): ")
+if message.islower() | key.islower() == True:
+    print("The message or key that you enter contains lowercase letters. Please follow the instructions.")
+else:
 # convert the key to numbers
+    num_key = [ord(char) - ord('A') for char in key]
 # convert the plaintext to numbers
 # encrypt the message
 #addition and modulus computation
