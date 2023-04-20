b4bc0f5593d888976959b164f1f88036b63da860
jasminericka <jasmin.celebre@gmail.com>
2023-04-21 06:04:52 +0800

    asked for user input

---
+++ vigenere_cipher.py
@@ -0,0 +1,11 @@
+#jasmin ericka celebre
+#problem 3 the vigenere cipher
+
+#ask user for message and key then save it
+message = input("Please type the message(all uppercase letters, no spaces): ").strip()
+key = input("Please type the key (all uppercase letters): ")
+# convert the key to numbers
+# convert the plaintext to numbers
+# encrypt the message
+#addition and modulus computation
+# print the output
